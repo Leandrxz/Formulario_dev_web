b907d013d50614088f738c525ca4fd5d12fe6911
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2024-09-23 18:04:15 -0300

    Components Added

---
+++ src/app/components/patient/patient-form/emergency-contact/emergency-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emergency-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './emergency-contact.component.html',
+  styleUrl: './emergency-contact.component.scss'
+})
+export class EmergencyContactComponent {
+
+}
---
+++ src/app/components/patient/patient-form/health-info/health-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-health-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './health-info.component.html',
+  styleUrl: './health-info.component.scss'
+})
+export class HealthInfoComponent {
+
+}
