55510733bce78bc0673435d7a249db9026df004b
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2024-09-23 18:33:55 -0300

    Added imports

--- src/app/components/patient/patient-form/patient-form.component.ts
+++ src/app/components/patient/patient-form/patient-form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { EmergencyContactComponent } from './emergency-contact/emergency-contact.component';
+import { HealthInfoComponent } from './health-info/health-info.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
 @Component({
   selector: 'app-patient-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactInfoComponent,
+    EmergencyContactComponent,
+    HealthInfoComponent,
+    PersonalInfoComponent,
+  ],
   templateUrl: './patient-form.component.html',
-  styleUrl: './patient-form.component.scss'
+  styleUrl: './patient-form.component.scss',
 })
-export class PatientFormComponent {
-
-}
+export class PatientFormComponent {}
