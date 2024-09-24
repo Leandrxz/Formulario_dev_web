39e8d9039f1f72c3edf0e3c7c92bfd87a1d49500
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2024-09-23 21:41:32 -0300

    Section Header created

--- src/app/components/patient/patient-form-header/patient-form-header.component.ts
+++ src/app/components/patient/patient-form-header/patient-form-header.component.ts
@@ -5,8 +5,31 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './patient-form-header.component.html',
-  styleUrl: './patient-form-header.component.scss'
+  styleUrl: './patient-form-header.component.scss',
 })
 export class PatientFormHeaderComponent {
+  dynamicClockHours: string = '';
+  dynamicClockMinutes: string = '';
+  dynamicClockSeconds: string = '';
 
+  /**
+   * This function creates a dynamic digital clock to be displayed in the form's header section
+   * @returns {void}
+   */
+  dynamicClock = setInterval((): void => {
+    // Taking the Time according to the system's (user) time
+    let dateToday: Date = new Date();
+    let hours: string = dateToday.getHours().toString();
+    let minutes: string = dateToday.getMinutes().toString();
+    let seconds: string = dateToday.getSeconds().toString();
+
+    // Adding a 0 in the beggining of the time if < 10, to be like 09
+    if (parseInt(hours) < 10) hours = '0' + hours;
+    if (parseInt(minutes) < 10) minutes = '0' + minutes;
+    if (parseInt(seconds) < 10) seconds = '0' + seconds;
+
+    this.dynamicClockHours = hours;
+    this.dynamicClockMinutes = minutes;
+    this.dynamicClockSeconds = seconds;
+  }, 1000);
 }
